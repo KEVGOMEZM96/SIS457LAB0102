58fc61c6493c9a4021de03b132156215e440c60f
Kevin Gomez Mollo <kevinnatsugomez1234@gmail.com>
2022-04-04 02:53:12 -0400

    first commit

---
+++ principal.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char v = 'v';
+	char s = 's';
+
+
+	while (v == 'v') {
+
+		cout << "Mi nombre es Kevin Gomez Mollo" << endl;
+		cout << "Tengo 25 años" << endl;
+		cout << "Naci el 21 de Julio de 1996,en Buenos Aires Argentina" << endl;
+		cout << "Mis padres son Valentin Gomez Tito y Eva Mollo Benites " << endl;
+		cout << "Tengo 4 hermanas llamadas Erika,Nicole,Valentina y Sasha" << endl;
+		cout << "Tengo un solo abuelo llamado Ramon Gomez" << endl;
+		cout << "Tengo una novia llamada Maria Luz" << endl;
+		cout << "Tengo doble nacionalidad tanto argrntina como boliviana" << endl;
+		cout << "Actualmente vivo en la Ciudad de Sucre" << endl;
+		cout << "Estudio la carrera de Ingenieria de Sistemas" << endl;
+		cout << "Mis pasastiempos favoritos son salir a comer ,ir a lugares tranquilos y jugar algunos videojuegos" << endl;
+		cout << "Mi color favorito es el color verde" << endl;
+		cout << "Mi club favorito de futbol es Boca Jr" << endl;
+		cout << "Uno de mis sueños es poder viajar ,conocer lugares y comer comidas exoticas" << endl;
+		cout << "Actualmente estoy en el tercer cuerso de la materia de sis457" << endl;
+		cout << "Desea volver a ver la biografía oprima la (v/V) Para salir oprima la (s/S) para salir" << endl;
+		cin >> v;
+
+	}
+	return 0;
+}
+
